01ea6f10c9aefebe4f524847a20b95ff72557f05
Elle Nash <56604081+v0idx@users.noreply.github.com>
2020-08-13 07:51:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,40 @@ import requests
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-def readFile(environ):
-    link = "https://code.v0idx.com/files" + environ['PATH_INFO']
-    text = requests.get(link)
-    ret = text.text.replace('<','&lt').replace('>','&gt')
-    return ret
+def getCode(subPath):
+    if subPath != '/':
+        file = "/home/*******/code.v0idx.com/files"
+        file += subPath
+        
+        with open(file,'r') as f:
+            content = f.readlines()
+            f.close()
+    else:
+        content = [""]
+    return ''.join(content).replace('<','&lt').replace('>','&gt')
+
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    text = []
-    text.append("<!DOCTYPE HTML><html><head>")
-    text.append("<link rel='stylesheet' href='https://code.v0idx.com/styling/dracula.css'><title>code</title> <script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js'></script><script>hljs.initHighlightingOnLoad();</script>")
-    text.append("</head><pre><code>")
-    if environ['PATH_INFO'] != '/':
-        text.append(readFile(environ))
-    text.append("</code></pre></html>")
-
-    response = ''.join(text)
+    resp = []
+    resp.append("<!DOCTYPE HTML>")
+    resp.append("<html><head>")
+    resp.append("<title>")
+    resp.append(environ['PATH_INFO'][1::])
+    resp.append("</title>")
+    resp.append("<link rel='stylesheet' href='https://code.v0idx.com/styling/materialdark.css'>")
+    resp.append("<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js'></script>")
+    resp.append("<script>hljs.initHighlightingOnLoad();</script>")
+    resp.append("</head>")
+    resp.append("<body>")
+    resp.append("<pre>")
+    resp.append("<code>")
+    resp.append(getCode(environ['PATH_INFO']))
+    resp.append("</code>")
+    resp.append("</pre>")
+    resp.append("</body>")
+    resp.append("</html>")
     
-    return [response.encode()]
+    text = ''.join(resp)
+    
+    return [text.encode()]
